6f0a3e3396f08fa78c6ecb8b7eede3fdfa106bc5
JiovannyMH <114841637+JiovannyMH@users.noreply.github.com>
2022-12-05 22:52:56 -0600

    Update cliente.js

--- models/cliente.js
+++ models/cliente.js
@@ -13,7 +13,7 @@ var esquema = new mongoose.Schema({
         fotoPerfil:             {type: String},
         creacion:               {type: Date, default: Date.now},
         modificacion:           {type: Date, default: Date.now},
-        rol:                    {type: String, required: false},
+        rol:                    {type: String, default: "cliente"},
 });
 
 module.exports = mongoose.model('clientes', esquema);
